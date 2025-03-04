d3fc484a51c042befa8107b0735f14d709045bf7
Sourabit2 <souravsaha111000@gmail.com>
2025-03-04 15:48:06 +0000

    added VM properties and commented Storage account block

--- VM_provisioning/main.tf
+++ VM_provisioning/main.tf
@@ -20,46 +20,66 @@ resource "azurerm_resource_group" "rg" {
   location = var.az_location
 }
 */
-resource "azurerm_virtual_network" "myVnet"{
-  resource_group_name=var.az_resource_group_name
-  name=var.az_virtualnetwork_name
-  address_space=var.az_virtualnetwork_adress_space
-  location=var.az_location
+resource "azurerm_virtual_network" "myVnet" {
+  resource_group_name = var.az_resource_group_name
+  name                = var.az_virtualnetwork_name
+  address_space       = var.az_virtualnetwork_adress_space
+  location            = var.az_location
 }
-resource "azurerm_subnet" "subnet"{
-  name=var.az_vnet_subnet_name
-  address_prefixes=var.az_vnet_subnet_address_prefixes
-  resource_group_name=var.az_resource_group_name
-  virtual_network_name=var.az_virtualnetwork_name
+resource "azurerm_subnet" "subnet" {
+  name                 = var.az_vnet_subnet_name
+  address_prefixes     = var.az_vnet_subnet_address_prefixes
+  resource_group_name  = var.az_resource_group_name
+  virtual_network_name = var.az_virtualnetwork_name
 }
-resource "azurerm_network_interface" "mynic"{
-  name=var.az_network_interface_name
-  location=var.az_location
-  resource_group_name=var.az_resource_group_name
+resource "azurerm_network_interface" "mynic" {
+  name                = var.az_network_interface_name
+  location            = var.az_location
+  resource_group_name = var.az_resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                    = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
-resource "azurerm_public_ip" "publicip"{
-  name=var.az_public_ip
-  location=var.az_location
-  resource_group_name=var.az_resource_group_name
-  allocation_method="Dynamic"
+resource "azurerm_public_ip" "publicip" {
+  name                = var.az_public_ip
+  location            = var.az_location
+  resource_group_name = var.az_resource_group_name
+  allocation_method   = "Dynamic"
 }
-resource "azurerm_network_interface_ip_configuration" "nic_ip_config"{
-  name="ipconfig"
-  network_interface_id=azurerm_network_interface.mynic.id
-  public_ip_address_id=azurerm_public_ip.publicip.id
-  private_ip_address_allocation=var.az_private_ip_allocation
+resource "azurerm_network_interface_ip_configuration" "nic_ip_config" {
+  name                          = "ipconfig"
+  network_interface_id          = azurerm_network_interface.mynic.id
+  public_ip_address_id          = azurerm_public_ip.publicip.id
+  private_ip_address_allocation = var.az_private_ip_allocation
 }
-resource "azurerm_linux_virtual_machine" "vm"{
-  
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.az_vm_name
+  resource_group_name = var.az_resource_group_name
+  location            = var.az_location
+  size                = var.az_vm_size
+  admin_username      = var.az_admin_username
+  admin_password      = var.az_admin_password
+  network_interface_ids = [
+    azurerm_network_interface.mynic.id,
+  ]
+  os_disk {
+    name                      = var.az_os_disk_name
+    caching                   = var.az_os_disk_caching
+    create_option             = var.az_os_disk_create_option
+    os_disk_managed_disk_type = var.az_os_disk_managed_disk_type
+  }
+  source_image_reference {
+    publisher = var.az_image_publisher
+    offer     = var.az_image_offer
+    sku       = var.az_image_sku
+    version   = var.az_image_version
+  }
 }
 # Storage Account
-resource "azurerm_storage_account" "sa" {
+/*resource "azurerm_storage_account" "sa" {
   name                     = var.az_storage_account_name
   resource_group_name      = var.az_storage_account_rg_name
   location                 = var.az_location
@@ -70,11 +90,11 @@ resource "azurerm_storage_account" "sa" {
     environment = "az_sa_mslearn"
   }
 }
-
+resource 
 # Storage Container
 resource "azurerm_storage_container" "container" {
   name                  = "content"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = var.az_container_access_type
 }
-#sdafsdf
+*/
--- VM_provisioning/variables.tf
+++ VM_provisioning/variables.tf
@@ -3,18 +3,18 @@ variable "az_location" {
   type    = string
   default = "westus"
 }
-variable "az_resource_group_name"{
-  type=string
-  default="myownrg"
+variable "az_resource_group_name" {
+  type    = string
+  default = "myownrg"
 }
 variable "az_subscription_id" {
-  type    = string
+  type = string
 }
 
 #STORAGE Account variables
 variable "az_storage_account_type" {
   type    = string
-  default = "Standard" 
+  default = "Standard"
 }
 variable "az_storage_account_name" {
   type    = string
@@ -24,53 +24,65 @@ variable "az_storage_account_replication_type" {
   type    = string
   default = "lrs"
 }
-variable "az_container_access_type"{
-  type=string
-  default="blob"
+variable "az_container_access_type" {
+  type    = string
+  default = "blob"
 }
 
 #Network variables
-variable "az_virtualnetwork_name"{
-  type=string
-  default="vnet1"
+variable "az_virtualnetwork_name" {
+  type    = string
+  default = "vnet1"
 }
-variable "az_virtualnetwork_adress_space"{
-  type=string
-  default=["10.0.0.1/16"]
+variable "az_virtualnetwork_adress_space" {
+  type    = string
+  default = ["10.0.0.1/16"]
 }
-variable "az_vnet_subnet_name"{
-  type=string
-  default="subnet1"
+variable "az_vnet_subnet_name" {
+  type    = string
+  default = "subnet1"
 }
-variable "az_vnet_subnet_address_prefixes"{
-  type=string
-  default=["10.0.1.0/24"]
+variable "az_vnet_subnet_address_prefixes" {
+  type    = string
+  default = ["10.0.1.0/24"]
 }
-variable "az_vnet_subnet_address_prefixes"{
-  type=string
-  default=["10.0.1.0/24"]
+variable "az_vnet_subnet_address_prefixes" {
+  type    = string
+  default = ["10.0.1.0/24"]
 }
-variable "az_network_interface_name"{
-  type=string
-  default="nic1"
+variable "az_network_interface_name" {
+  type    = string
+  default = "nic1"
 }
-variable "az_public_ip"{
-  type=string
-  default="public_ip"
+variable "az_public_ip" {
+  type    = string
+  default = "public_ip"
 }
-variable "az_private_ip_allocation"{
-  type=string
-  default="Dynamic"
+variable "az_private_ip_allocation" {
+  type    = string
+  default = "Dynamic"
 }
 
 #VIRTUAL Machine VARIABLES
-variable "az_vm_name"{
-  type=string
-  default="vm1"
+variable "az_vm_name" {
+  type    = string
+  default = "vm1"
 }
-variable "az_vm_size"{
-  type=string
-  default="Standard_DS1_v2"
+variable "az_vm_size" {
+  type    = string
+  default = "Standard_DS1_v2"
+}
+variable "az_admin_username" {
+  type        = string
+  description = "The admin username for the VM."
+  default     = "adminuser"
+}
+
+variable "az_admin_password" {
+  type        = string
+  description = "The admin password for the VM. **IMPORTANT:** Store securely."
+  sensitive   = true # Mark as sensitive to prevent output in logs
+  default     = "Password1234!"
 }
 #STORAGE OS DISK VARIABLES
 variable "az_os_disk_name" {
@@ -137,19 +149,6 @@ variable "az_computer_name" {
   type        = string
   description = "The computer name for the VM."
   default     = "mycomputer"
-}
-
-variable "az_admin_username" {
-  type        = string
-  description = "The admin username for the VM."
-  default     = "adminuser"
-}
-
-variable "admin_password" {
-  type        = string
-  description = "The admin password for the VM. **IMPORTANT:** Store securely."
-  sensitive   = true # Mark as sensitive to prevent output in logs
-  default     = "Password1234!"
 }
 
 variable "disable_password_auth" {
