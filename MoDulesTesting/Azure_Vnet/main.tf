2e21c51bf84d1736799f1f0f75ce427a739be659
SouravTheNerd <souravbittu1996@gmail.com>
2025-03-11 04:29:52 +0000

    added child and root modules

---
+++ MoDulesTesting/Azure_Vnet/main.tf
@@ -0,0 +1,26 @@
+# Example Azure Terraform Module: Virtual Network (VNet)
+
+# modules/vnet/main.tf
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_prefixes
+}
+
+
+
---
+++ MoDulesTesting/Azure_Vnet/variables.tf
@@ -0,0 +1,40 @@
+
+# modules/vnet/variables.tf
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be created."
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network."
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet."
+  type        = string
+  default     = "default"
+}
+
+variable "subnet_prefixes" {
+  description = "The address prefixes for the subnet."
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the resources."
+  type        = map(string)
+  default     = {}
+}
