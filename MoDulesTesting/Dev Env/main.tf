a5d2cc1bfaede2282342eb14d7281bbd2041f71f
Sourabit2 <souravsaha111000@gmail.com>
2025-03-12 15:43:49 +0000

    final refinement done to Dev Vnet, test went fine

--- MoDulesTesting/Dev Env/main.tf
+++ MoDulesTesting/Dev Env/main.tf
@@ -3,13 +3,14 @@
 # Dev Env/main.tf
 
 module "dev_vnet" {
-  source              = "/workspaces/TerraForm-Projects/TerraForm-Projects/MoDulesTesting"
-  resource_group_name = "dev-rg"
-  location            = "East US"
-  vnet_name           = "dev-vnet"
-  address_space       = ["10.1.0.0/16"]
-  subnet_name         = "dev-subnet"
-  subnet_prefixes     = ["10.1.1.0/24"]
+  source                 = "/home/sourav/TESTING/gittest/TerraForm-Projects/MoDulesTesting/Azure_Vnet"
+  az_resource_group_name = "learn-105d915f-4d0b-46de-a92e-6c10f68ef0b0" #Dynamic
+  az_location            = "East US"
+  az_vnet_name           = "dev-vnet"
+  az_address_space       = ["10.1.0.0/16"]
+  az_subnet_name     = "dev-subnet" #not needed
+  az_subnet_prefixes = ["10.1.1.0/24"]
+  az_subscription_id = "f26515d9-3bc6-4a50-8192-cb816a833f54"
   tags = {
     Environment = "dev"
     Project     = "MyApp"
